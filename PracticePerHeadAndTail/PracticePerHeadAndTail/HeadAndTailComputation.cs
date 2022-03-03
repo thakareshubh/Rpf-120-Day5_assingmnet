e869e38c64d5d31f15798697a51e56691f116139
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:21:43 +0530

    Added coin flip problem

---
+++ PracticePerHeadAndTail/PracticePerHeadAndTail/HeadAndTailComputation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePerHeadAndTail
+{
+    internal class HeadAndTailComputation
+    {
+        public static int head=0;
+        public static int tail=0;
+        
+        ///computation of % of head and tail
+        
+        public void compute(int usernumber)
+        {
+            Random rand = new Random();
+            while (usernumber!=0)
+            {
+                double r1=rand.NextDouble();
+                if(r1<0.5)
+                {
+                    tail=tail+1;
+
+                }
+                else
+                {
+                    head=head+1;
+                }
+                usernumber--;
+            }
+            int temp1 = (head * 100) / usernumber;
+
+            int temp2 = (tail * 100) / usernumber;
+            Console.WriteLine("% of head :"+temp1);
+            Console.WriteLine("% of tail :"+temp2);
+        }
+    }
+}
---
+++ PracticePerHeadAndTail/PracticePerHeadAndTail/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticePerHeadAndTail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int usernumber=Convert.ToInt32(Console.ReadLine());
+            HeadAndTailComputation P1=new HeadAndTailComputation();
+            P1.compute(usernumber);
+
+        }
+    }
+}
