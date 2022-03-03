d46d7416700d88a9efa1d447170058d2ec1dac9e
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 11:39:06 +0530

    Added SwapTwonumber

---
+++ PracticeSwapTwoNumbers/PracticeSwapTwoNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticeSwapTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two number ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            SwapingOfTwoNumber p1=new SwapingOfTwoNumber();
+            p1.swap(a,b);
+        }
+    }
+}
---
+++ PracticeSwapTwoNumbers/PracticeSwapTwoNumbers/SwapingOfTwoNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSwapTwoNumbers
+{
+    internal class SwapingOfTwoNumber
+    {
+        public void swap(int a,int b)
+        {
+            int temp=a;
+            a=b;
+            b=temp;
+            Console.WriteLine("value of a is :" + a + "value of b is :" + b );
+        }
+    }
+}
