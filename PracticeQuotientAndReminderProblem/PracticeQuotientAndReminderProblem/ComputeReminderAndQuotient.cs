28bd2061d6698195c9fa46f35a5b3a35657d34af
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:36:38 +0530

    Added reminder quatient problem

---
+++ PracticeQuotientAndReminderProblem/PracticeQuotientAndReminderProblem/ComputeReminderAndQuotient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuotientAndReminderProblem
+{
+    internal class ComputeReminderAndQuotient
+    {
+        public void compute(int divident,int divisor)
+        {
+            int reminder;
+            int quotient=Math.DivRem(divident, divisor, out reminder);
+            Console.WriteLine("quotient is :"+quotient+""+"reminder is :"+reminder);
+        }
+    }
+}
---
+++ PracticeQuotientAndReminderProblem/PracticeQuotientAndReminderProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticeQuotientAndReminderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter divident and divisor");
+            int divident=Convert.ToInt32(Console.ReadLine());
+            int divisor=Convert.ToInt32(Console.ReadLine());
+            ComputeReminderAndQuotient p1=new ComputeReminderAndQuotient();
+            p1.compute(divident, divisor);
+        }
+    }
+}
