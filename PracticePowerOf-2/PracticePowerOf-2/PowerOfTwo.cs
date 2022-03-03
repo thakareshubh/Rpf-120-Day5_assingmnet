574ec6003c49534665ab584d83712190a556a77c
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:30:33 +0530

    Added power of two problem

---
+++ PracticePowerOf-2/PracticePowerOf-2/PowerOfTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePowerOf_2
+{
+    internal class PowerOfTwo
+    {
+        
+        
+        public static void  ComputePowerTable(int number)
+        {
+            int a = 2^number;
+            int i=2;
+            int p = 1;
+            while(i<=a)
+            {
+                i = p * i;
+                Console.WriteLine($"Table of Power of 2 :{i}");
+                i*=2;
+            }
+                    
+        }
+    }
+}
---
+++ PracticePowerOf-2/PracticePowerOf-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticePowerOf_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number upto which you want to print number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            PowerOfTwo.ComputePowerTable(number);
+        }
+    }
+}
