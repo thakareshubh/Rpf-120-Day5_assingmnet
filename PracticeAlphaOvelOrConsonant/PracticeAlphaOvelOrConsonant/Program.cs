89fab73dd6615f1112c87e080aa9e551c2fbc0bd
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 11:48:47 +0530

    Added alpha consonant problem

---
+++ PracticeAlphaOvelOrConsonant/PracticeAlphaOvelOrConsonant/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PracticeAlphaOvelOrConsonant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter alphabet");
+           char al=Convert.ToChar(Console.ReadLine());
+            FindCoAndVo p1 = new FindCoAndVo();
+            p1.find(al);
+
+        }
+    }
+    public class FindCoAndVo
+    { 
+ 
+        public void  find(char al)
+        {
+            if(al=='a' || al=='e'|| al=='i' || al== 'o' || al=='u' || al=='A' || al=='E' || al=='I' || al=='O' || al=='U')
+            {
+                Console.WriteLine(al + " is alphabet");
+            }
+            else
+            {
+                Console.WriteLine(al + " is consonant");
+            }
+                
+            
+        }
+    }
+}
