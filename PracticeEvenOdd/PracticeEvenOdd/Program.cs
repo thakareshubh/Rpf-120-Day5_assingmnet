844c867b7d52a9a08dff998f525ae367764e500c
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 11:43:39 +0530

    Added Even Odd number

---
+++ PracticeEvenOdd/PracticeEvenOdd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PracticeEvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            PracticeEvenOdd p1=new PracticeEvenOdd();
+            p1.Evenodd(number);
+            
+        }
+    }
+
+    public class PracticeEvenOdd
+    {
+        public void Evenodd(int number)
+        {
+            if(number%2==0)
+            {
+                Console.WriteLine(number + " :is even number");
+
+            }
+            else
+            {
+                Console.WriteLine(number + " :is odd number");
+            }
+        }
+    }
+}
