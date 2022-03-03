f0e8edf37a95e906c94b9d6ed38eb64c08a7b2f4
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:28:40 +0530

    Added leap year program

---
+++ PracticeLeapYear/PracticeLeapYear/ComputationOfYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLeapYear
+{
+    internal class ComputationOfYear
+    {
+        public static int Compute(int year)
+        {
+            if(year%4==0 && year/100!=0 && year%400==0 )
+            {
+                Console.WriteLine(year + "is leap year");
+                
+            }
+            else
+            {
+                Console.WriteLine(year + "is not leap year");
+            }
+            return year;
+        }
+    }
+}
---
+++ PracticeLeapYear/PracticeLeapYear/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeLeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year here");
+            int year = Convert.ToInt32(Console.ReadLine());
+            ComputationOfYear.Compute(year);
+        }
+    }
+}
