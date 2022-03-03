e1e6180c85c7f3e7c0010748e8bf78a41d810952
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:34:28 +0530

    Added factors of number program

---
+++ PracticeFactorOfNumber/PracticeFactorOfNumber/FactorizationOfNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFactorOfNumber
+{
+    internal class FactorizationOfNumber
+    {
+        public static int n = 1;   
+        public static void Factorized(int number)
+        {
+            while(number > 0)
+            {
+                
+                n *= number;
+                number--;
+
+            }
+            Console.WriteLine($"Factorization of number is {n}");
+        }
+    }
+}
---
+++ PracticeFactorOfNumber/PracticeFactorOfNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PracticeFactorOfNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            FactorizationOfNumber.Factorized(number);
+        }
+    }
+}
