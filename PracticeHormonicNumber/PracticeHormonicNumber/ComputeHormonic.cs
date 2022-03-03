4eb8f5f12444fc8faee692aad7f426dbdafc045a
Shubham Thakare <shubhamthakare032@gmail.com>
2022-03-03 10:32:42 +0530

    Added harmonic number problem

---
+++ PracticeHormonicNumber/PracticeHormonicNumber/ComputeHormonic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeHormonicNumber
+{
+    internal class ComputeHormonic
+    {
+        public void Compute(int number)
+        {
+            
+            int r1= 1;
+            int i = 1;
+            while(i < number)
+            {
+                r1 += 1 / i;
+                i++;
+            }
+            Console.WriteLine(r1);
+            
+        }
+    }
+}
---
+++ PracticeHormonicNumber/PracticeHormonicNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PracticeHormonicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            ComputeHormonic p1=new ComputeHormonic();
+            p1.Compute(number);
+        }
+    }
+}
